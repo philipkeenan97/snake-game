048561a9b68f68ac3941a58572407582643e3ee2
philipkeenan97 <pkeenan97@gmail.com>
2021-08-23 21:29:07 +0100

    controll snake with arrows and included game end conditions

--- snakegame.js
+++ snakegame.js
@@ -11,6 +11,7 @@ let snake = [
   {x: 170, y: 200},
   {x: 160, y: 200}
 ]
+let changing_Direction = false;
 let dx = 10;
 let dy = 0;
 
@@ -39,7 +40,56 @@ function moveSnake() {
       snake.pop();
 }
 
+function change_Direction() {
+
+      const LEFT_KEY = 37;
+      const RIGHT_KEY = 39;
+      const UP_KEY = 38;
+      const DOWN_KEY = 40;
+
+      if (changing_Direction) return;
+      changing_Direction = true;
+      const keyPressed = event.keyCode;
+      const goingUp = dy === -10;
+      const goingDown = dy === 10;
+      const goingRight = dx === 10;
+      const goingLeft = dx === -10;
+
+      if (keyPressed === LEFT_KEY && !goingRight) {
+            dx = -10;
+            dy = 0;
+      }
+
+      if (keyPressed === UP_KEY && !goingDown) {
+            dx = 0;
+            dy = -10;
+      }
+
+      if (keyPressed === RIGHT_KEY && !goingLeft) {
+            dx = 10;
+            dy = 0;
+      }
+
+      if (keyPressed === DOWN_KEY && !goingUp) {
+            dx = 0;
+            dy = 10;
+      }
+}
+
+function hasGameEnded() {
+      for (let i = 4; i < snake.length; i++) {
+            if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+      }
+      const hitLeftWall = snake[0].x < 0;
+      const hitRightWall = snake[0].x > snakeboard.width - 10;
+      const hitTopWall = snake[0].y < 0;
+      const hitBottomWall = snake[0].y > snakeboard.height - 10;
+      return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+}
+
 function main() {
+      if (hasGameEnded()) return;
+      changing_Direction = false;
       setTimeout(function onTick() {
       clearCanvas();
       moveSnake();
@@ -49,3 +99,4 @@ function main() {
 }
 
 main();
+document.addEventListener("keydown", change_Direction);
