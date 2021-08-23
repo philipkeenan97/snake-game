49aef4300bed60685f9b1948bb2030cfe4a8751e
philipkeenan97 <pkeenan97@gmail.com>
2021-08-23 22:12:13 +0100

    formatting of tabs/line differences

--- snakegame.js
+++ snakegame.js
@@ -52,12 +52,10 @@ function moveSnake() {
 }
 
 function change_Direction() {
-
       const LEFT_KEY = 37;
       const RIGHT_KEY = 39;
       const UP_KEY = 38;
       const DOWN_KEY = 40;
-
       if (changing_Direction) return;
       changing_Direction = true;
       const keyPressed = event.keyCode;
@@ -65,22 +63,18 @@ function change_Direction() {
       const goingDown = dy === 10;
       const goingRight = dx === 10;
       const goingLeft = dx === -10;
-
       if (keyPressed === LEFT_KEY && !goingRight) {
             dx = -10;
             dy = 0;
       }
-
       if (keyPressed === UP_KEY && !goingDown) {
             dx = 0;
             dy = -10;
       }
-
       if (keyPressed === RIGHT_KEY && !goingLeft) {
             dx = 10;
             dy = 0;
       }
-
       if (keyPressed === DOWN_KEY && !goingUp) {
             dx = 0;
             dy = 10;
